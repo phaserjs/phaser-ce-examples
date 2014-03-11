09ff0f76b9dbbe54a918b48a8f49c1318ef94c0e
photonstorm <rich@photonstorm.com>
2014-03-11 22:44:27 +0000

    Initial move of demos from main phaser repo to phaser-examples.

---
+++ tile sprites/colliding with tiling sprite.js
@@ -0,0 +1,59 @@
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+var ball;
+var tilesprite;
+var cursors;
+
+function preload() {
+
+    game.load.image('starfield', 'assets/misc/starfield.jpg');
+    game.load.image('ball', 'assets/sprites/pangball.png');
+
+}
+
+function create() {
+
+    ball = game.add.sprite(400, 0, 'ball');
+    ball.physicsEnabled = true;
+
+    tilesprite = game.add.tileSprite(300, 450, 200, 100, 'starfield');
+
+    tilesprite.physicsEnabled = true;
+    // tilesprite.body.immovable = true;
+
+    // tilesprite.body.setRectangle(200, 100, 0, 0);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+}
+
+function update() {
+
+    if (cursors.left.isDown)
+    {
+        tilesprite.x -= 8;
+        tilesprite.tilePosition.x -= 8;
+    }
+    else if (cursors.right.isDown)
+    {
+        tilesprite.x += 8;
+        tilesprite.tilePosition.x += 8;
+    }
+
+    if (cursors.up.isDown)
+    {
+        tilesprite.tilePosition.y += 8;
+    }
+    else if (cursors.down.isDown)
+    {
+        tilesprite.tilePosition.y -= 8;
+    }
+
+}
+
+function render() {
+
+    // game.debug.physicsBody(tilesprite);
+
+}
