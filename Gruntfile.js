33ad383192edc9b660dc0123961f5e6e9c7c5e07
photonstorm <rich@photonstorm.com>
2014-03-14 08:43:46 +0000

    Added gitignore, updated examples.js, fixed version check.

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadTasks('./tasks');
+
+    grunt.initConfig({
+
+        examples: {
+            all: {
+            options: {
+                base: 'examples',
+                excludes: ['_site', 'assets', 'states', 'wip']
+            },
+            src: ['examples/**/*.js'],
+            dest: 'examples/_site/examples.json'
+            }
+        },
+
+        connect: {
+            root: {
+                options: {
+                    keepalive: true,
+                    hostname: '*'
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('default', ['examples']);
+
+};
