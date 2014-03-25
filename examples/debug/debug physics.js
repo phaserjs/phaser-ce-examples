c9ab2e840794760ce59239ebec6615f7a0fd97c8
photonstorm <rich@photonstorm.com>
2014-03-25 12:50:52 +0000

    Fixed the old debug-physics example.

--- examples/debug/debug physics.js
+++ examples/debug/debug physics.js
@@ -1,54 +1,37 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update:update, render:render  });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render  });
 
-var sprite, otherSprite ;
-var counter = 0 ;
-var step = Math.PI * 2 / 360 ;
-
+var sprite;
+var counter = 0;
+var step = Math.PI * 2 / 360;
 
 function preload() {
 
-    // Load images to use as the game sprites
     game.load.image('sprite', 'assets/sprites/phaser2.png');
-    game.load.image('otherSprite', 'assets/sprites/phaser-dude.png');
 
 }
 
 function create() {
 
-    // Create sprite A and put it in the middle of the stage
-    sprite = game.add.sprite(0, 0, 'sprite');
-    sprite.alpha = 0.5 ;
-    sprite.x = game.width / 2 ;
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    // create sprite B
-    otherSprite = game.add.sprite(0, 0, 'otherSprite');
-    otherSprite.alpha = 0.5 ;
-    otherSprite.x = (game.width / 2) + 150 ;
-    otherSprite.y = (game.height / 2) + 150 ;
-    
-    
+    sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'sprite');
+    sprite.anchor.set(0.5);
 
-    game.physics.enable([sprite,otherSprite], Phaser.Physics.ARCADE);
-
+    game.physics.arcade.enable(sprite);
     
 }
 
 function update()
 {
-
     // Move sprite up and down smoothly for show
-    var tStep = Math.sin( counter ) ;
-    sprite.y = (game.height/2) + tStep * 30 ;
-    sprite.rotation += Phaser.Math.degToRad( 0.1 * tStep ) ;
-    counter += step ;
+    var tStep = Math.sin(counter);
+    sprite.body.y = 120 + tStep * 60;
+    counter += step;
 }
 
 function render() {
 
-    // Physics
-    game.debug.spriteBounds(sprite);
-
-    game.debug.spriteBounds(otherSprite);
+    game.debug.body(sprite);
 
 }
