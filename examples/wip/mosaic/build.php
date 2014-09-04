9cc26fd34f2a9ea97e37da519da82f74f5a3aeff
photonstorm <rich@photonstorm.com>
2014-09-04 15:25:45 +0100

    Mosaic work.

---
+++ examples/wip/mosaic/build.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Mosaic</title>
+        <script src="jquery-2.0.3.min.js" type="text/javascript"></script>
+        <script src="mosaic-fx.js" type="text/javascript"></script>
+    </head>
+    <body>
+
+        <canvas id="fx" width="994" height="454" />
+
+        <script type="text/javascript">
+
+            var config = { 
+                canvas: 'fx', 
+                pics: ['mosaic_guide_Zakiyya.jpg', 'mosaic_guide_Vijan.jpg', 'pic1.jpg', 'pic2.jpg'],
+                tileWidth: 14, 
+                tileHeight: 19, 
+                duration: { min: 250, max: 1000 },
+                delay: { min: 0, max: 5000 },
+                callbacks: {
+                    loadComplete: onLoad,
+                    transitionStart: startTransition,
+                    transitionEnd: endTransition
+                }
+            };
+
+            function onLoad(fx) {
+
+                fx.start();
+
+            }
+
+            function startTransition(fx, img) {
+
+                console.log('Starting to display', img);
+
+                //  Note - you can change any of the transition parameters at this point, they will take effect in time to influence this transition
+                fx.durationMin = fx.between(0, 100);
+                fx.durationMax = fx.between(100, 500);
+
+            }
+
+            function endTransition(fx, img) {
+
+                console.log('Ending display of', img);
+
+                //  Move onto the next one - the value given here is how long (in milliseconds) to wait until starting the next transition
+                fx.next(1000);
+                
+            }
+
+            var fx = new FX(config);
+
+        </script>
+
+    </body>
+</html>
