129f2344437f27660499151292a0fce4610819b3
photonstorm <rich@photonstorm.com>
2014-12-09 23:16:21 +0000

    Removed direct body set.

--- examples/games/breakout.js
+++ examples/games/breakout.js
@@ -84,7 +84,7 @@ function update () {
     //  Fun, but a little sea-sick inducing :) Uncomment if you like!
     // s.tilePosition.x += (game.input.speed.x / 2);
 
-    paddle.body.x = game.input.x;
+    paddle.x = game.input.x;
 
     if (paddle.x < 24)
     {
