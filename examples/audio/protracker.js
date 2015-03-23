bb6e278485e94aebfee9f84561f27484a58e7bc4
photonstorm <rich@photonstorm.com>
2015-03-23 11:55:46 +0000

    Fixed to use crop instead of width. Sorted out the messy formatting.

--- examples/audio/protracker.js
+++ examples/audio/protracker.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 var mods = [];
-var current = -1;
+var current = 0;
 
 var vumeter = [];
 var channels = [];
@@ -10,101 +10,116 @@ var module;
 
 function preload() {
 
-  game.load.script('protracker', '_plugins/ProTracker.js');
+    game.load.script('protracker', '_plugins/ProTracker.js');
 
-  game.load.image('vu', 'assets/sprites/vu.png');
-  game.load.image('logo', 'assets/sprites/soundtracker.png');
-  game.load.image('bg', 'assets/skies/sky2.png');
-  game.load.image('vulkaiser', 'assets/pics/vulkaiser_red.png');
+    game.load.image('vu', 'assets/sprites/vu.png');
+    game.load.image('logo', 'assets/sprites/soundtracker.png');
+    game.load.image('bg', 'assets/skies/sky2.png');
+    game.load.image('vulkaiser', 'assets/pics/vulkaiser_red.png');
 
-  game.load.binary('shampoo', 'assets/audio/protracker/shampoo.mod', modLoaded, this);
-  game.load.binary('macrocosm', 'assets/audio/protracker/macrocosm.mod', modLoaded, this);
-  game.load.binary('impulse', 'assets/audio/protracker/act_of_impulse.mod', modLoaded, this);
-  game.load.binary('enigma', 'assets/audio/protracker/enigma.mod', modLoaded, this);
-  game.load.binary('elysium', 'assets/audio/protracker/elysium.mod', modLoaded, this);
-  game.load.binary('stardust', 'assets/audio/protracker/sd-ingame1.mod', modLoaded, this);
-  game.load.binary('globaltrash', 'assets/audio/protracker/global_trash_3_v2.mod', modLoaded, this);
+    game.load.binary('shampoo', 'assets/audio/protracker/shampoo.mod', modLoaded, this);
+    game.load.binary('macrocosm', 'assets/audio/protracker/macrocosm.mod', modLoaded, this);
+    game.load.binary('impulse', 'assets/audio/protracker/act_of_impulse.mod', modLoaded, this);
+    game.load.binary('enigma', 'assets/audio/protracker/enigma.mod', modLoaded, this);
+    game.load.binary('elysium', 'assets/audio/protracker/elysium.mod', modLoaded, this);
+    game.load.binary('stardust', 'assets/audio/protracker/sd-ingame1.mod', modLoaded, this);
+    game.load.binary('globaltrash', 'assets/audio/protracker/global_trash_3_v2.mod', modLoaded, this);
 
 }
 
 function modLoaded(key, data) {
-  mods.push(key);
-  var buffer = new Uint8Array(data);
-  return buffer;
+
+    mods.push(key);
+    var buffer = new Uint8Array(data);
+    return buffer;
+
 }
 
 function load_next_module()
 {
-  current==mods.length-1?current=0:current++;
+    current == mods.length - 1 ? current = 0 : current++;
 
-  module.stop();
-  module.clearsong();
+    module.stop();
+    module.clearsong();
 
-  module.buffer = game.cache.getBinary(mods[current]);
-  module.parse();
+    module.buffer = game.cache.getBinary(mods[current]);
+    module.parse();
 
-  // BUG if width==0
-  for (i=0; i<vumeter.length; i++) { vumeter[i].width=1; }
+    // BUG if width==0
+    // for (var i = 0; i < vumeter.length; i++)
+    // {
+    //     vumeter[i].width = 1;
+    // }
+
 }
 
 function create() {
 
-  game.add.sprite(0, 0, 'bg');
-  game.add.sprite(500, 32, 'logo');
-  game.add.sprite(580, 371, 'vulkaiser');
+    game.add.sprite(0, 0, 'bg');
+    game.add.sprite(500, 32, 'logo');
+    game.add.sprite(580, 371, 'vulkaiser');
 
-  for (i=0, y=200; i<4; i++, y+=50) {
-    vumeter[i] = game.add.sprite(400, y, 'vu');
-  }
+    for (var i = 0, y = 200; i < 4; i++, y += 50)
+    {
+        vumeter[i] = game.add.sprite(400, y, 'vu');
+        vumeter[i].crop(new Phaser.Rectangle(0, 0, 300, 30));
+    }
 
-  module = new Protracker();
+    module = new Protracker();
 
-  //module.play() has to be called from a callback
-  module.onReady = function() {
-    module.play();
-  };
+    //module.play() has to be called from a callback
+    module.onReady = function() {
+        module.play();
+    };
 
-  game.input.onDown.add(load_next_module, this);
+    module.buffer = game.cache.getBinary(mods[current]);
+    module.parse();
+
+    game.input.onDown.add(load_next_module, this);
 
 }
-
 
 function update() {
-  //  module.sample = array of Objects containing informations about a played sample
-  
-  for (i=0; i<vumeter.length; i++) {
 
-    if (module.channel[i])
+    //  module.sample = array of Objects containing informations about a played sample
+
+    for (var i = 0; i < vumeter.length; i++)
     {
-      var smp_index = module.channel[i].sample;
-      channels[i] = { sample_index:smp_index,
-                      sample_name:module.sample[smp_index].name };
+        if (module.channel[i])
+        {
+            var smp_index = module.channel[i].sample;
+            channels[i] = { sample_index:smp_index, sample_name: module.sample[smp_index].name };
 
-      var w = Math.round(module.vu[i] * 1200);
-      //you have to check that width is > 0 !
-      vumeter[i].width = w>0?w:1;
+            var w = Math.round(module.vu[i] * 1200);
 
+            if (w > 300)
+            {
+                w = 300;
+            }
+
+            vumeter[i].cropRect.width = w;
+            vumeter[i].updateCrop();
+        }
     }
-  }
-}
 
+}
 
 function render() {
 
-  for (i=0, y=32; i<vumeter.length; i++, y+=32) {
-
-    if (channels[i])
+    for (var i = 0, y = 32; i < vumeter.length; i++, y += 32)
     {
-      game.debug.text('Channel #' + i + ' : sample ' + channels[i].sample_index + '  ' + channels[i].sample_name, 16,y);
-      game.debug.text('vu' + i + ':' + module.vu[i], 16, 350+y);
+        if (channels[i])
+        {
+            game.debug.text('Channel #' + i + ' : sample ' + channels[i].sample_index + '  ' + channels[i].sample_name, 16, y);
+            // game.debug.text('vu' + i + ': ' + module.vu[i], 16, 350 + y);
+        }
     }
 
-  }
+    game.debug.text('Position: ' + module.position, 16, 160);
+    game.debug.text('Pattern: ' + module.row, 16, 192);
+    game.debug.text('BPM: ' + module.bpm, 16, 224);
+    game.debug.text('Speed: ' + module.speed, 16, 256);
+    game.debug.text('Name: ' + module.title, 16, 288);
+    game.debug.text('Signature: ' + module.signature, 16, 320);
 
-  game.debug.text('Position: ' + module.position, 16, 160);
-  game.debug.text('Pattern: ' + module.row, 16, 192);
-  game.debug.text('BPM: ' + module.bpm, 16, 224);
-  game.debug.text('Speed: ' + module.speed, 16, 256);
-  game.debug.text('Name: ' + module.title, 16, 288);
-  game.debug.text('Signature: ' + module.signature, 16, 320);
 }
