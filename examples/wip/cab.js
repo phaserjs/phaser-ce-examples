2430a07c3ced917c90cf173fd70d67c0969883d1
photonstorm <rich@photonstorm.com>
2015-07-30 17:19:46 +0100

    New examples and tests.

---
+++ examples/wip/cab.js
@@ -0,0 +1,34 @@
+
+// var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', this);
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', this);
+
+function preload() {
+
+    game.load.image('phaser', 'assets/sprites/phaser.png');
+
+}
+
+var myGroup;
+
+function create() {
+
+    myGroup = this.game.add.group();
+
+    myGroup.create(50, 50, "phaser");
+    myGroup.create(250, 50, "phaser");
+    var s = myGroup.create(250, 150, "phaser");
+    s.angle = 45;
+
+    // setInterval(function () {
+    //     console.log('cab', myGroup.cacheAsBitmap);
+    //     myGroup.cacheAsBitmap = !myGroup.cacheAsBitmap;
+        // console.log(mySprite.position);
+    // }, 1000);
+
+    game.input.onDown.add(gen, this);
+
+}
+
+function gen() {
+    myGroup.cacheAsBitmap = !myGroup.cacheAsBitmap;
+}
