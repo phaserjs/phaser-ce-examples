087011b3b124f3b8ae69ba1d6161b374ff854914
Andres Kalle <mjomble@gmail.com>
2016-04-08 17:08:48 +0300

    Code cleanup in "tween an image"

--- examples/basics/07 tween an image.js
+++ examples/basics/07 tween an image.js
@@ -1,9 +1,4 @@
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-var tween;
-var prev = -400;
-var t = Date.now();
-var sprite;
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
 
 function preload() {
 
@@ -16,62 +11,18 @@ function preload() {
 
     //  The second parameter is the URL of the image (relative)
     game.load.image('einstein', 'assets/pics/ra_einstein.png');
-    // game.load.image('einstein', 'assets/sprites/chunk.png');
 }
 
 function create() {
 
-    sprite = game.add.sprite(-400, 0, 'einstein');
-    // var sprite = game.add.sprite(0, 0, 'einstein');
+    var sprite = game.add.sprite(-400, 0, 'einstein');
 
     //  Here we create a tween on the sprite created above
-    tween = game.add.tween(sprite);
+    var tween = game.add.tween(sprite);
 
     //  The object defines the properties to tween.
-    //  In this case it will move to x 600
-    //  The 6000 is the duration in ms - 6000ms = 6 seconds
-    // tween.to({ x: 500, y: 500 }, 5000, 'Linear', true, 2000);
+    //  In this case it will move to x 800
+    //  The 5000 is the duration in ms - 5000ms = 5 seconds
     tween.to({ x: 800 }, 5000, 'Linear', true, 0);
-
-    //  And this starts it going
-    // tween.start();
-
-}
-
-function update() {
-
-    if (sprite.x < 800)
-    {
-        // sprite.x += 4;
-    }
-
-}
-
-function render() {
-
-    return;
-
-    if (sprite.x < 800)
-    {
-        var ms = Date.now() - t;
-
-        var d = window.dx - prev;
-
-        if (ms > 20)
-        {
-            console.log('------------>', ms, d);
-        }
-        else
-        {
-            console.log(ms, d);
-        }
-
-        prev = window.dx;
-
-        t = Date.now();
-    }
-
-    // game.debug.text(this.game.time.elapsedMS, 32, 32);
-    // game.debug.text(window.dx, 32, 32);
 
 }
