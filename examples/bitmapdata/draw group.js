79ffd65e47d7ba44b6148472d9c441f2a0e9e3b4
photonstorm <rich@photonstorm.com>
2016-06-14 16:06:02 +0100

    Updated example.

--- examples/bitmapdata/draw group.js
+++ examples/bitmapdata/draw group.js
@@ -1,5 +1,6 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+// var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
 function preload() {
 
@@ -8,6 +9,8 @@ function preload() {
     game.load.image('squad', 'assets/sprites/bsquadron3.png');
     game.load.image('loop', 'assets/sprites/beball1.png');
     game.load.bitmapFont('desyrel', 'assets/fonts/bitmapFonts/desyrel.png', 'assets/fonts/bitmapFonts/desyrel.xml');
+    game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
+    game.load.atlasJSONHash('bot', 'assets/sprites/running_bot.png', 'assets/sprites/running_bot.json');
 
 }
 
@@ -20,7 +23,12 @@ function create() {
     //  Add a bunch of sprites in random positions to the group
     for (var i = 0; i < 16; i++)
     {
-        group.create(game.world.randomX, game.world.randomY, 'loop');
+        var sprite = group.create(game.world.randomX, game.world.randomY, 'loop');
+
+        if (i % 2 === 1)
+        {
+            sprite.tint = 0xff00ff;
+        }
     }
 
     var bmpText = game.make.bitmapText(32, 64, 'desyrel', 'Bitmap Text in the Group', 64);
@@ -28,12 +36,20 @@ function create() {
     group.add(bmpText);
 
     //  A sprite with nested children
-    var s = group.create(150, 300, 'disk');
+    var s = group.create(250, 300, 'disk');
     var r = game.make.sprite(32, 16, 'squad');
     s.addChild(r);
 
     r.angle = 45;
     s.scale.x = -1;
+    s.angle = -20;
+
+    var mummy = group.create(600, 4, 'mummy', 8);
+    mummy.scale.set(4);
+    mummy.smoothed = false;
+
+    var bot = group.create(60, 200, 'bot');
+    bot.scale.set(2);
 
     //  This is the BitmapData we're going to be drawing to
     var bmd = game.add.bitmapData(game.width, game.height);
@@ -43,7 +59,7 @@ function create() {
     game.stage.updateTransform();
 
     //  Draw the group to the BitmapData
-    bmd.drawGroup(group);
-    // bmd.drawFull(group);
+    // bmd.drawGroup(group);
+    bmd.drawGroup(game.world);
 
 }
