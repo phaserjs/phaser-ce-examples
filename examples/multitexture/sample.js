65637b855a8a3273f562840d62e3a064b0e0ac05
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2016-07-25 15:59:39 -0400

    multi texture batching example

---
+++ examples/multitexture/sample.js
@@ -0,0 +1,20 @@
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+
+function preload() {
+
+    game.load.image('treasure_trap', 'assets/sprites/treasure_trap.png');
+    game.load.image('bunny', 'assets/sprites/bunny.png');
+    game.load.image('cokecan', 'assets/sprites/cokecan.png');
+    game.load.image('ilkke', 'assets/sprites/ilkke.png');
+}
+
+function create() {
+    game.renderer.setTexturePriority(['treasure_trap', 'bunny', 'cokecan', 'ilkke']);
+
+    //  This simply creates a sprite using the mushroom image we loaded above and positions it at 200 x 200
+    var spriteA = window.spriteA = game.add.sprite(0, 0, 'treasure_trap');
+    var spriteB = window.spriteB = game.add.sprite(127, 0, 'bunny');
+    var spriteC = window.spriteB = game.add.sprite(500, 0, 'cokecan');
+    var spriteD = window.spriteB = game.add.sprite(580, 0, 'ilkke');
+}
