d72802e74a83590fa25b7e5b957c2964354d5e8a
Richard Davey <rdavey@gmail.com>
2016-08-25 20:13:55 +0100

    Sprite sheet from Graphics.

--- examples/arcade physics/group vs self.js
+++ examples/arcade physics/group vs self.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
 
-	game.load.spritesheet('spinner', 'assets/sprites/bluemetal_32x32x4.png', 32, 32);
+    game.load.spritesheet('spinner', 'assets/sprites/bluemetal_32x32x4.png', 32, 32);
 
 }
 
@@ -11,27 +11,27 @@ var sprites;
 
 function create() {
 
-	//	Here we create a group, populate it with sprites, give them all a random velocity
-	//	and then check the group against itself for collision
+    //  Here we create a group, populate it with sprites, give them all a random velocity
+    //  and then check the group against itself for collision
 
-	sprites = game.add.physicsGroup(Phaser.Physics.ARCADE);
+    sprites = game.add.physicsGroup(Phaser.Physics.ARCADE);
 
-	for (var i = 0; i < 90; i++)
-	{
-		var s = sprites.create(game.rnd.integerInRange(100, 700), game.rnd.integerInRange(32, 200), 'spinner');
-		s.animations.add('spin', [0, 1, 2, 3]);
-		s.play('spin', 20, true);
-		s.body.velocity.set(game.rnd.integerInRange(-200, 200), game.rnd.integerInRange(-200, 200));
-	}
+    for (var i = 0; i < 90; i++)
+    {
+        var s = sprites.create(game.rnd.integerInRange(100, 700), game.rnd.integerInRange(32, 200), 'spinner');
+        s.animations.add('spin', [0, 1, 2, 3]);
+        s.play('spin', 20, true);
+        s.body.velocity.set(game.rnd.integerInRange(-200, 200), game.rnd.integerInRange(-200, 200));
+    }
 
-	sprites.setAll('body.collideWorldBounds', true);
-	sprites.setAll('body.bounce.x', 1);
-	sprites.setAll('body.bounce.y', 1);
+    sprites.setAll('body.collideWorldBounds', true);
+    sprites.setAll('body.bounce.x', 1);
+    sprites.setAll('body.bounce.y', 1);
 
 }
 
 function update() {
 
-	game.physics.arcade.collide(sprites);
+    game.physics.arcade.collide(sprites);
 
 }
