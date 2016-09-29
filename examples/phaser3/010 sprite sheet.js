adeb6bf0724f728048e72d02c9a7e49d9575e5fd
photonstorm <rich@photonstorm.com>
2016-09-29 16:32:41 +0100

    New example.

---
+++ examples/phaser3/010 sprite sheet.js
@@ -0,0 +1,60 @@
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, render: render });
+
+function preload() {
+
+    game.load.image('monster', 'assets/sprites/metalslug_monster39x40.png');
+
+}
+
+var manager;
+var texture;
+var frame;
+var f = 0;
+var x = 0;
+var y = 0;
+var anchor = new Phaser.Point();
+
+function create() {
+
+    manager = new Phaser.TextureManager(game);
+
+    texture = manager.create('monster', game.cache.getImage('monster'));
+
+    Phaser.TextureManager.Parsers.SpriteSheet(texture, 39, 40);
+
+    frame = texture.get(0);
+
+    // console.log(texture);
+    // console.log(texture.frames);
+    console.log(frame);
+
+}
+
+function render () {
+
+    var dx = frame.x - anchor.x * frame.width;
+    var dy = frame.y - anchor.y * frame.height;
+
+    game.context.drawImage(
+        frame.source,
+        frame.cutX,
+        frame.cutY,
+        frame.cutWidth,
+        frame.cutHeight,
+        x + dx,
+        y + dy,
+        frame.cutWidth,
+        frame.cutHeight
+    );
+
+    f++;
+
+    if (f === 15)
+    {
+        f = 0;
+    }
+
+    frame = texture.get(f);
+
+}
