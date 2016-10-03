6b32c7a39ca85bdcb7534800005247b15f848cd6
photonstorm <rich@photonstorm.com>
2016-10-03 17:15:00 +0100

    Yeehaa :)

---
+++ examples/phaser3/014 canvas renderer 1.js
@@ -0,0 +1,18 @@
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, xrender: render });
+
+function preload() {
+
+    game.load.image('einstein', 'assets/pics/ra_einstein.png');
+
+}
+
+function create() {
+
+    game.add.sprite(0, 0, 'einstein');
+
+}
+
+function render () {
+
+}
