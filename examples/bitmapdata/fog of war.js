ea4ffae2d21a6b245307a459c006cdd00ed6bc66
photonstorm <rich@photonstorm.com>
2017-08-18 16:30:23 +0100

    Updated fog of war
    [deploy: production]

--- examples/bitmapdata/fog of war.js
+++ examples/bitmapdata/fog of war.js
@@ -4,19 +4,27 @@ var fringe;
 var fogCircle;
 var cursors;
 var speed = 4;
+var player;
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload ()
 {
-    game.load.image('pic', 'assets/pics/Bounty_Hunter_by_Anathematixs_Desire.png');
+    game.load.image('map', 'assets/misc/ccmap.png');
+    game.load.image('plane', 'assets/sprites/ww2-plane.png');
 }
 
 function create ()
 {
-    game.add.image(0, 0, 'pic');
+    game.world.resize(2208, 1368);
 
-    fogCircle = new Phaser.Circle(800, 800, 1600);
+    game.add.image(0, 0, 'map');
+
+    //  A random player that will move around the map, the fog follows the player, not the camera
+    player = game.add.image(300, 400, 'plane');
+    player.anchor.setTo(0.5);
+
+    fogCircle = new Phaser.Circle(800, 800, 800);
 
     fringe = 64;
 
@@ -25,41 +33,57 @@ function create ()
 
     updateFogOfWar();
 
-    bmd.addToWorld();
+    var fogSprite = bmd.addToWorld();
+
+    fogSprite.fixedToCamera = true;
 
     cursors = game.input.keyboard.createCursorKeys();
+
+    var tween = game.add.tween(player).to({ x: 2000, y: 800 }, 15000, "Linear", true, 0, -1, true);
+
+    tween.onLoop.add(function (sprite, tween) {
+        sprite.scale.x *= -1
+    }, 0, this);
 }
 
 function update ()
 {
-    if (cursors.left.isDown)
-    {
-        fogCircle.x -= speed;
-        updateFogOfWar();
-    }
-    else if (cursors.right.isDown)
-    {
-        fogCircle.x += speed;
-        updateFogOfWar();
-    }
+    fogCircle.x = player.x;
+    fogCircle.y = player.y;
 
     if (cursors.up.isDown)
     {
-        fogCircle.y -= speed;
-        updateFogOfWar();
+        game.camera.y -= 4;
     }
     else if (cursors.down.isDown)
     {
-        fogCircle.y += speed;
-        updateFogOfWar();
+        game.camera.y += 4;
     }
+
+    if (cursors.left.isDown)
+    {
+        game.camera.x -= 4;
+    }
+    else if (cursors.right.isDown)
+    {
+        game.camera.x += 4;
+    }
+
+    updateFogOfWar();
 }
 
 function updateFogOfWar ()
 {
-    var gradient = bmd.context.createRadialGradient(fogCircle.x, fogCircle.y, fogCircle.radius, fogCircle.x, fogCircle.y, fogCircle.radius - fringe);
+    var gradient = bmd.context.createRadialGradient(
+        fogCircle.x - game.camera.x,
+        fogCircle.y - game.camera.y,
+        fogCircle.radius,
+        fogCircle.x - game.camera.x,
+        fogCircle.y - game.camera.y,
+        fogCircle.radius - fringe
+    );
 
-    gradient.addColorStop(0, 'rgba(255,255,255,0.8');
+    gradient.addColorStop(0, 'rgba(0,0,0,0.8');
     gradient.addColorStop(0.4, 'rgba(0,0,0,0.5');
     gradient.addColorStop(1, 'rgba(0,0,0,0');
 
