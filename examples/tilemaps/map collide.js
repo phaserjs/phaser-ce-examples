5f1ad037ca914dde5207642a15f49040624e7ceb
Richard Davey <rich@photonstorm.com>
2017-10-24 13:05:24 +0100

    Removed scaling.

--- examples/tilemaps/map collide.js
+++ examples/tilemaps/map collide.js
@@ -35,8 +35,6 @@ function create() {
     
     layer = map.createLayer('World1');
 
-    layer.scale.set(0.5);
-
     //  Un-comment this on to see the collision tiles
     // layer.debug = true;
 
